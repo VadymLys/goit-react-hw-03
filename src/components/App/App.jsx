24ec1115ff5dfacb91a38ce66e88dcc4e7ea7b78
Vadym <vadymlysytskyi@gmail.com>
2024-03-29 12:21:18 +0100

    2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,11 +3,10 @@ import "modern-normalize";
 import css from "../App/App.module.css";
 import SearchBox from "../SearchBox/SearchBox";
 import PhoneBook from "../PhoneBook/PhoneBook";
-import users from "./users.json";
 import ContactList from "../ContactList/ContactList";
 
 const App = () => {
-  const [contacts, setContacts] = useState(null);
+  const [contacts, setContacts] = useState("");
   const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
@@ -21,21 +20,20 @@ const App = () => {
     window.localStorage.setItem("contacts", JSON.stringify(contacts));
   }, [contacts]);
 
-  const filteredContacts =
-    contacts &&
-    contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(searchValue.toLowerCase())
-    );
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(searchValue.toLowerCase())
+  );
 
   const handleSearch = (evt) => {
-    setSearchValue(() => evt.target.value);
+    setSearchValue(evt.target.value);
   };
+
   return (
     <div className={css.container}>
       <h1>Phonebook</h1>
       <PhoneBook />
       <SearchBox searchValue={searchValue} onChange={handleSearch} />
-      <ContactList users={users} />
+      <ContactList users={filteredContacts} />
     </div>
   );
 };
