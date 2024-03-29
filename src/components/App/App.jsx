ef83e0f19f9028ec39b82f35d5973183566b9a75
Vadym <vadymlysytskyi@gmail.com>
2024-03-29 14:20:44 +0100

    3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "modern-normalize";
+import userData from "./users.json";
 import css from "../App/App.module.css";
 import SearchBox from "../SearchBox/SearchBox";
 import PhoneBook from "../PhoneBook/PhoneBook";
 import ContactList from "../ContactList/ContactList";
 
 const App = () => {
-  const [contacts, setContacts] = useState("");
-  const [searchValue, setSearchValue] = useState("");
+  const [contacts, setContacts] = useState(() =>
+    JSON.parse(window.localStorage.getItem("contacts") || JSON.parse(userData))
+  );
 
-  useEffect(() => {
-    const storedContacts = window.localStorage.getItem("contacts");
-    if (storedContacts) {
-      setContacts(JSON.parse(storedContacts));
-    }
-  }, []);
+  const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
     window.localStorage.setItem("contacts", JSON.stringify(contacts));
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,6 +2,7 @@ import Contact from "../Contact/Contact";
 import css from "../ContactList/ContactList.module.css";
 
 const ContactList = ({ users }) => {
+  console.log(users);
   return (
     <ul className={css.contactList}>
       {users.map((user) => (
